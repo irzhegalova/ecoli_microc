74d9d1a3ccb20e7000d72ade8e4f7f9b755e84d2
Irina Zhegalova <ir.zhegalova@gmail.com>
2024-05-12 00:03:40 +0300

    May 11, 2024 RedC bw, working on Topo, RNAP, SMC chipseqs

--- scripts/pins.py
+++ scripts/pins.py
@@ -34,7 +34,7 @@ cm = 1/2.54  # centimeters in inches
 
 # %% hairpins
 df = pd.read_excel('data/hairpins_Gavrilov.xlsx')
-hairpins_df = pd.DataFrame({
+hairpins_df_Pseudobedpe = pd.DataFrame({
     'chrom1': 'NC_000913.3',
     'start1': df.wt_middle//25*25,
     'end1': (df.wt_middle//25+1)*25,
@@ -42,8 +42,76 @@ hairpins_df = pd.DataFrame({
     'start2': df.wt_middle//25*25,
     'end2': (df.wt_middle//25+1)*25,
 })
-# hairpins_df.to_csv('data/hairpins_25.bedpe', sep='\t', index=False, header=False)
+
+hairpins_df = pd.DataFrame({
+    'chrom': 'NC_000913.3',
+    'start': df.wt_middle//25*25,
+    'end': (df.wt_middle//25+1)*25,
+})
+
+hairpins_df_bedpe = pd.DataFrame({
+    'chrom1': 'NC_000913.3',
+    'start1': df.wt_left//25*25,
+    'end1': (df.wt_left//25+1)*25,
+    'chrom2': 'NC_000913.3',
+    'start2': df.wt_right//25*25,
+    'end2': (df.wt_right//25+1)*25,
+})
+hairpins_df_bedpe.to_csv('data/hairpins_25.bedpe', sep='\t', index=False, header=False)
+
+
+hairpins_df_Pseudobedpe.to_csv('data/hairpins_25.pseudo.bedpe', sep='\t', index=False, header=False)
+# hairpins_df.to_csv('data/hairpins_25.bed', sep='\t', index=False, header=False)
 hairpins_df.head()
+
+# %% create bins
+resolution = 100
+clr = cooler.Cooler('data/wt.combined.MG1655.mapq30.25.mcool::/resolutions/' + str(resolution))
+df_chromsizes = clr.chromsizes
+bins = cooler.binnify(df_chromsizes, 25)
+bins.chrom = bins.chrom.astype(str)
+fasta_records = bf.load_fasta('data/genome.fasta')
+view_df_chromsizes = clr.chromsizes.reset_index()
+view_df_chromsizes.columns = ['chrom', 'end']
+view_df_chromsizes['start'] = 0
+view_df_chromsizes['name'] = 'NC_000913.3'
+view_df_chromsizes = view_df_chromsizes[['chrom', 'start', 'end', 'name']]
+
+# %%
+resolution = 100
+flank = 100
+stack = cooltools.pileup(clr, hairpins_df, view_df=view_df_chromsizes, 
+flank=flank, nproc=2)
+# Mirror reflect snippets when the feature is on the opposite strand
+# mask = np.array(sites.strand == '-', dtype=bool)
+# stack[:, :, mask] = stack[::-1, ::-1, mask]
+
+# %%
+# Aggregate. Note that some pixels might be converted to NaNs after IC, thus we aggregate by nanmean: 
+mtx = np.nanmean(stack, axis=2)
+
+# Load colormap with large number of distinguishable intermediary tones,
+# The "fall" colormap in cooltools is exactly for this purpose.
+# After this step, you can use "fall" as cmap parameter in matplotlib:
+import cooltools.lib.plotting
+
+plt.imshow(
+    np.log10(mtx),
+    vmin = -3,
+    vmax = -1,
+    cmap='fall',
+    interpolation='none')
+
+plt.colorbar(label = 'log10 mean ICed Hi-C')
+ticks_pixels = np.linspace(0, flank*2//resolution,5)
+ticks_kbp = ((ticks_pixels-ticks_pixels[-1]/2)*resolution//1000).astype(int)
+plt.xticks(ticks_pixels, ticks_kbp)
+plt.yticks(ticks_pixels, ticks_kbp)
+plt.xlabel('relative position, kbp')
+plt.ylabel('relative position, kbp')
+
+plt.show()
+
 
 # %% chromosight
 # chromosight quantify --pattern hairpins --threads=4 data/hairpins_25.bedpe data/wt.combined.MG1655.mapq30.25.mcool::/resolutions/25 hairping.AGav
@@ -51,14 +119,6 @@ hairpins_df.head()
 
 # %% download Regulon db
 # !bash scripts/chipseq_mine.sh
-
-# %% create bins
-resolution = 500
-clr = cooler.Cooler('data/wt.combined.MG1655.mapq30.25.mcool::/resolutions/' + str(resolution))
-df_chromsizes = clr.chromsizes
-bins = cooler.binnify(df_chromsizes, 25)
-bins.chrom = bins.chrom.astype(str)
-fasta_records = bf.load_fasta('data/genome.fasta')
 
 # %% 337 - 350, 360-364 bad (no coordinates)
 for id in range(363, 364):
@@ -143,3 +203,48 @@ fig.savefig('results/chipseq.map_mean.pdf', dpi=100, bbox_inches='tight')
 fig.show()
 fig.clf()
 # %%
+from scipy.stats import zscore
+redC_nascent_df = bf.read_table('data/wt_merged.rna1.10k.bg', schema='bedGraph')
+redC_nascent_df['chrom'] = 'NC_000913.3'
+redC_nascent_df['z_value'] = zscore(redC_nascent_df.value.tolist())
+bf.to_bigwig(df=redC_nascent_df.loc[:,['chrom', 'start', 'end', 'value']], chromsizes=df_chromsizes,
+            outpath='data/wt_merged.rna1.10k.chromName.bw',
+            path_to_binary='/home/fox/micromamba/envs/omics_env/bin/bedGraphToBigWig')
+
+bf.to_bigwig(df=redC_nascent_df.loc[:,['chrom', 'start', 'end', 'z_value']], chromsizes=df_chromsizes,
+            outpath='data/wt_merged.rna1.10k.zscore.bw',
+            path_to_binary='/home/fox/micromamba/envs/omics_env/bin/bedGraphToBigWig')
+# %%
+redC_total_df = bf.read_table('data/wt_merged.rna1.total.bg', schema='bedGraph')
+redC_total_df['chrom'] = 'NC_000913.3'
+redC_total_df['z_value'] = zscore(redC_total_df.value.tolist())
+bf.to_bigwig(df=redC_total_df.loc[:,['chrom', 'start', 'end', 'value']], chromsizes=df_chromsizes,
+            outpath='data/wt_merged.rna1.total.chromName.bw',
+            path_to_binary='/home/fox/micromamba/envs/omics_env/bin/bedGraphToBigWig')
+
+bf.to_bigwig(df=redC_total_df.loc[:,['chrom', 'start', 'end', 'z_value']], chromsizes=df_chromsizes,
+            outpath='data/wt_merged.rna1.total.zscore.bw',
+            path_to_binary='/home/fox/micromamba/envs/omics_env/bin/bedGraphToBigWig')
+# %%
+nbins=81
+plot_around_loop(
+        path_bw='data/wt_merged.rna1.10k.zscore.bw',
+        plot_name="RedC.10k.zscore.pdf",
+        nbins=61,
+        resolution=200,
+        chrom_file=df_chromsizes,
+        window=5000,
+        mode="median",
+        ymin=-2,
+        ymax=2,
+        vmin=-3,
+        vmax=3,
+        norm=False,
+        fill=True,
+        how_far_from_edge=25,
+        bed_list=[
+            "data/hairpins_25.bedpe",
+        ],
+        pic_path='results/',
+        return_matrix=False
+    )
